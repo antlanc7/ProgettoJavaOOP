fd03d465363de57762a9c50db668e4fc5561128e
armentpelivani <48828177+armentpelivani@users.noreply.github.com>
2019-06-03 11:05:32 +0200

    inizializzato progetto

---
+++ src/main/java/com/lancarm/ProgettoJavaOOP/ProgettoJavaOopApplication.java
@@ -0,0 +1,13 @@
+package com.lancarm.ProgettoJavaOOP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoJavaOopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoJavaOopApplication.class, args);
+	}
+
+}
