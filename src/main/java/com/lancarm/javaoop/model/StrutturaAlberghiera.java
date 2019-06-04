de4dcab9b7500d4102cd0fc61c6335680612ab14
armentpelivani <48828177+armentpelivani@users.noreply.github.com>
2019-06-04 18:44:55 +0200

    Creata classe alberghi

---
+++ src/main/java/com/lancarm/javaoop/model/StrutturaAlberghiera.java
@@ -0,0 +1,51 @@
+package com.lancarm.javaoop.model;
+
+public class StrutturaAlberghiera {
+    private String insegna,categoria,indirizzo,municipio,tipologia;
+    private int camere;
+
+    public StrutturaAlberghiera(String insegna, String categoria, String indirizzo, String municipio, String tipologia, int camere) {
+        this.insegna = insegna;
+        this.categoria = categoria;
+        this.indirizzo = indirizzo;
+        this.municipio = municipio;
+        this.tipologia = tipologia;
+        this.camere = camere;
+    }
+
+    public String getInsegna() {
+        return insegna;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public String getIndirizzo() {
+        return indirizzo;
+    }
+
+    public String getMunicipio() {
+        return municipio;
+    }
+
+    public String getTipologia() {
+        return tipologia;
+    }
+
+    public int getCamere() {
+        return camere;
+    }
+
+    @Override
+    public String toString() {
+        return "StrutturaAlberghiera{" +
+                "insegna='" + insegna + '\'' +
+                ", categoria='" + categoria + '\'' +
+                ", indirizzo='" + indirizzo + '\'' +
+                ", municipio='" + municipio + '\'' +
+                ", tipologia='" + tipologia + '\'' +
+                ", camere=" + camere +
+                '}';
+    }
+}
