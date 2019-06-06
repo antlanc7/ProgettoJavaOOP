49a5fdbac5a6d16e5c4e5fb8b21787e8555af10c
antlanc7 <antonio.lanciotti@live.it>
2019-06-06 16:58:30 +0200

    aggiunto metodo al controller

--- src/main/java/com/lancarm/javaoop/controller/StrAlbController.java
+++ src/main/java/com/lancarm/javaoop/controller/StrAlbController.java
@@ -1,7 +1,10 @@
 package com.lancarm.javaoop.controller;
 
+import com.lancarm.javaoop.model.StrutturaAlberghiera;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,9 +19,15 @@ public class StrAlbController {
     public StrAlbController(StrAlbService service) {
         this.service = service;
     }
+
     //metodi per la comunicazione con il client
     @GetMapping("/data")
     public List getAllData (){
         return service.getAllData();
+    }
+
+    @GetMapping("/data/{id}")
+    public StrutturaAlberghiera getStrAlbById(@PathVariable int id){
+        return service.getStrAlb(id);
     }
 }
