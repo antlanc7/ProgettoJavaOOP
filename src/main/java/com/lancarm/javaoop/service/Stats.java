1fc77d2e4a8eedcf70195c7e820ad6f93c41ef52
antlanc7 <antonio.lanciotti@live.it>
2019-06-13 16:04:21 +0200

    Aggiunta classe per statistiche

---
+++ src/main/java/com/lancarm/javaoop/service/Stats.java
@@ -0,0 +1,84 @@
+package com.lancarm.javaoop.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public abstract class Stats {
+
+    public static int count(List list){
+        return list.size();
+    }
+
+    public static double sum(List<Number> list){
+        double s = 0;
+        for (Number n : list){
+            s += n.doubleValue();
+        }
+        return s;
+    }
+
+    public static double avg(List<Number> list){
+        return sum(list)/count(list);
+    }
+
+    public static double max(List<Number> list){
+        double max = list.get(0).doubleValue();
+        for (Number n : list){
+            double nval = n.doubleValue();
+            if (nval>max) max = nval;
+        }
+        return max;
+    }
+
+    public static double min(List<Number> list){
+        double min = list.get(0).doubleValue();
+        for (Number n : list){
+            double nval = n.doubleValue();
+            if (nval<min) min = nval;
+        }
+        return min;
+    }
+
+    public static double std(List<Number> list){
+        double avg = avg(list);
+        double var = 0;
+        for (Number n : list){
+            var += Math.pow(n.doubleValue()-avg,2);
+        }
+        return Math.sqrt(var);
+    }
+
+    public static Map<Object,Integer> uniqueElements(List list){
+        Map<Object,Integer> map = new HashMap<>();
+        for (Object elem : list){
+            if (map.containsKey(elem)){
+                map.replace(elem,map.get(elem)+1);
+            }
+            else{
+                map.put(elem,1);
+            }
+        }
+        return map;
+    }
+
+    public static Map<String,Object> getAllStats(String fieldName, List list){
+        Map<String,Object> map = new HashMap<>();
+        map.put("field",fieldName);
+        if (!list.isEmpty()){
+            if (list.get(0) instanceof Number) {
+                map.put("avg", avg(list));
+                map.put("min", min(list));
+                map.put("max", max(list));
+                map.put("std", std(list));
+                map.put("sum", sum(list));
+                map.put("count", count(list));
+                return map;
+            }
+            else {
+                map.put("uniqueElements",uniqueElements(list));
+            }
+        }
+        return map;
+    }
+}
