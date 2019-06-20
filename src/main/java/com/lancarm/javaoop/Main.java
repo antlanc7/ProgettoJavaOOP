4ff7b78f56251bec7d1e56dc42beb59ae24b80b3
armentpelivani <48828177+armentpelivani@users.noreply.github.com>
2019-06-20 12:27:20 +0200

    Rinominata classe Main

--- src/main/java/com/lancarm/javaoop/Main.java
+++ src/main/java/com/lancarm/javaoop/Main.java
@@ -7,10 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Classe main che avvia il server Spring
  */
 @SpringBootApplication
-public class ProgettoJavaOopApplication {
+public class Main {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProgettoJavaOopApplication.class, args);
+        SpringApplication.run(Main.class, args);
     }
 
 }
