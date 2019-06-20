4b5960cd2ec22837b9a2a3bf4e86804e6b8f0962
armentpelivani <48828177+armentpelivani@users.noreply.github.com>
2019-06-20 12:26:20 +0200

    Javadoc MAIN

--- src/main/java/com/lancarm/javaoop/ProgettoJavaOopApplication.java
+++ src/main/java/com/lancarm/javaoop/ProgettoJavaOopApplication.java
@@ -3,6 +3,9 @@ package com.lancarm.javaoop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Classe main che avvia il server Spring
+ */
 @SpringBootApplication
 public class ProgettoJavaOopApplication {
 
